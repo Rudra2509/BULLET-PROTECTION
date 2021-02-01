1f5c6b33088ca19eba3ef5b18cbb8e71ed665b2d
Rudra2509 <76781821+Rudra2509@users.noreply.github.com>
2021-02-01 17:43:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -83,12 +83,4 @@ function draw() {
  
  drawSprites();
 }
-function hasCollided(bullet,walL){
-  bulletLeftEdge = bullet.x + bullet.width/2;
-  wallLeftEdge = wall.x - wall.width/2;
- if(bulletLeftEdge>=wallLeftEdge){
-  return true;
- }else {
-   return false;
- }
-}
+
